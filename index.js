304b465dacafbccdd504b24a995607fd3a634af6
corprodoc <corprodoc@gmail.com>
2025-07-24 15:30:26 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+import { PDFDocument, rgb, StandardFonts } from 'pdf-lib'
+import fetch from 'node-fetch'
+
+export default {
+  async fetch(request, env, ctx) {
+    try {
+      const { searchParams } = new URL(request.url)
+      const url = searchParams.get('url')
+      const headerText = searchParams.get('header') || ''
+
+      if (!url) {
+        return new Response('Missing "url" parameter', { status: 400 })
+      }
+
+      const pdfBytes = await fetch(url).then(res => res.arrayBuffer())
+      const pdfDoc = await PDFDocument.load(pdfBytes)
+
+      const font = await pdfDoc.embedFont(StandardFonts.Helvetica)
+      const pages = pdfDoc.getPages()
+
+      for (const page of pages) {
+        const { width, height } = page.getSize()
+        page.drawText(headerText, {
+          x: 50,
+          y: height - 50,
+          size: 14,
+          font,
+          color: rgb(0, 0, 0)
+        })
+      }
+
+      const newPdfBytes = await pdfDoc.save()
+      return new Response(newPdfBytes, {
+        headers: {
+          'Content-Type': 'application/pdf',
+          'Content-Disposition': 'inline; filename="header.pdf"'
+        }
+      })
+    } catch (e) {
+      return new Response('Error: ' + e.message, { status: 500 })
+    }
+  }
+}
